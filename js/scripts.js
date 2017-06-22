d700993df183ad328a9bab0494c504ab756f3879
Christine Callo and Jonathan Stein <jonathan@>
2017-06-21 20:46:27 -0700

    Moved consecutive consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,40 @@
 //business time
 var addWay = function(word) {
   var letters = word.split("");
+  var consecutiveConsonants = 0;
+  var final;
+
   if (letters[0] === "a" || letters[0] === "e" || letters[0] === "i" || letters[0] === "o" || letters[0] === "u") {
     var joined = letters.join("");
-    var final = "";
+
     final = final.concat(joined, "way");
-    console.log(final);
+
+  } else {
+
+    for (var i = 0; i < letters.length; i++) {
+
+        if (letters[i] !== "a" && letters[i] !== "e" && letters[i] !== "i" && letters[i] !== "o" && letters[i] !== "u"){
+          consecutiveConsonants++;
+          //then its a consonant!
+        }
+        else{
+          //found a vowel!
+          break;
+        }
+    }
+
+    var lastPart = letters.slice(consecutiveConsonants,letters.length).join('');
+    var firstPart = letters.slice(0,consecutiveConsonants).join(''); //takes first letter up to the nth letter
+
+    final = lastPart+firstPart+'ay';
   }
   return final;
 }
+
+
+
+
+
 
 //user interface
 $(function(){
--- readme.md
+++ readme.md
@@ -6,10 +6,6 @@ The program adds "way" to single-letter words beginning with a vowel.
 Example Input: i
 Example Output: iway
 
-The program treats 'y' as a consonant for words beginning with 'y'.
-Example Input: yes
-Example Output: esyay
-
 The program moves all consecutive consonants to the end of the word when the word starts with a consonant.
 Example Input: translator
 Example Output: anslatortray
@@ -17,3 +13,7 @@ Example Output: anslatortray
 The program treats 'u' as a consonant when it comes after 'q'.
 Example Input: Squeal
 Example Output: ealsquay
+
+The program treats 'y' as a consonant for words beginning with 'y'.
+Example Input: yes
+Example Output: esyay
